e7e02348e5f82e1d9982c18259fd2d448582320c
raj <raj@rajs-MacBook-Air.local>
2025-10-27 20:20:24 +0530

    rust version becuase zig sucks(unstable)

---
+++ oxidised/target/debug/deps/oxidised-7fda3348f5157326.d
@@ -0,0 +1,5 @@
+/Users/raj/projects/os_under1k/oxidised/target/debug/deps/oxidised-7fda3348f5157326.d: src/main.rs
+
+/Users/raj/projects/os_under1k/oxidised/target/debug/deps/liboxidised-7fda3348f5157326.rmeta: src/main.rs
+
+src/main.rs:
