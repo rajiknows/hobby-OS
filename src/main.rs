a62c6f4238b4306f4f55be5a57bb3cddfba7d7aa
Raj <rajeshhjhamain@gmail.com>
2025-12-31 14:29:16 +0530

    vga_buffer done

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{fmt::Write, panic::PanicInfo};
+
+mod macros;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
+// static HELLO: &[u8] = b"Hello world";
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    // clear_screen();
+    // println!("Hello World{}", "!");
+    // println!("sussy baka");
+    vga_buffer::WRITER.lock().write_str("hello raj!").unwrap();
+    panic!("Some panic message");
     loop {}
 }
-//
-// fn main() {
-//     println!("Hello, world!");
-// }
